18059e933b311df321b6b2bc9778befa22323071
Khoa Dư <khoa.dt6.5@gmail.com>
2024-03-03 00:55:36 +0700

    RESTful API

---
+++ My_WebAPI/My_WebAPI/Controllers/HangHoaController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using My_WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace My_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+
+        // Lấy ra danh sách
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // Trả về danh sách hàng hóa nếu thành công Ok()
+            return Ok(hangHoas);
+        }
+
+        // Lấy thêm phần id
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+                // LINQ [Object] Query
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    // NotFound trả về status 404
+                    return NotFound();
+                }
+                return Ok(hangHoa);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        // Thêm mới
+        [HttpPost]
+        public IActionResult Create(HangHoaVM hangHoaVM)
+        {
+            // Tạo object hàng hóa
+            var hanghoa = new HangHoa
+            {
+                MaHangHoa = Guid.NewGuid(),
+                TenHangHoa = hangHoaVM.TenHangHoa,
+                DonGia = hangHoaVM.DonGia
+            };
+            // object vào danh sách đã tạo ở trên 
+            hangHoas.Add(hanghoa);
+
+            return Ok(new
+            {
+                Success = true, Data = hanghoa
+            });
+        }
+
+        // Cập nhật
+        [HttpPut("{id}")]
+        public IActionResult Edit(string id, HangHoa hangHoaEdit)
+        {
+            try
+            {
+                // LINQ [Object] Query
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    // NotFound trả về status 404
+                    return NotFound();
+                }
+
+                if(id != hangHoa.MaHangHoa.ToString())
+                {
+                    return BadRequest();
+                }
+                // Update
+                hangHoa.TenHangHoa = hangHoaEdit.TenHangHoa;
+                hangHoa.DonGia = hangHoaEdit.DonGia;
+
+                return Ok();
+            }
+            catch
+            {
+                // Kiểm tra id theo chuỗi Guid 
+                return BadRequest();
+            }
+        }
+
+        // Xóa 
+        [HttpDelete("{id}")]
+        public IActionResult Remove(string id) 
+        {
+            try
+            {
+                // LINQ [Object] Query
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    // NotFound trả về status 404
+                    return NotFound();
+                }
+
+                if (id != hangHoa.MaHangHoa.ToString())
+                {
+                    return BadRequest();
+                }
+                // Update
+                hangHoas.Remove(hangHoa);
+
+                return Ok();
+            }
+            catch
+            {
+                // Kiểm tra id theo chuỗi Guid 
+                return BadRequest();
+            }
+        }
+
+    }
+}
---
+++ My_WebAPI/My_WebAPI/Models/HangHoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace My_WebAPI.Models
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa { get; set; }
+
+        public double DonGia { get; set; }
+    }
+
+    public class HangHoa : HangHoaVM
+    {
+        // Guid 
+        public Guid MaHangHoa { get; set; }
+    }
+}
